086655508d89e59be1e892c3ed7d2a1dc85d6de9
asma <asmanabila03@gmail.com>
2024-12-11 15:18:43 +0700

    form login view

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,10 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\LoginController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::resource('login', LoginController::class);
